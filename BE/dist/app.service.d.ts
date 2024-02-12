e959ca9763930dc8829923a5f3daf81ce1c5b7f1
Alekususenpai <aa_lara@live.com>
2024-02-11 23:05:11 -0500

    config files update

--- BE/dist/app.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { Repository } from 'typeorm';
-import { User } from './user.entity';
-export declare class AppService {
-    private usersRepository;
-    constructor(usersRepository: Repository<User>);
-    getHello(): Promise<string>;
-}
